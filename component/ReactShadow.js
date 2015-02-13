6205c8c9e2ae5036d6f9940d8ecc6b93d61892ca
Wildhoney <adam.timberlake@gmail.com>
2015-02-13 11:31:38 +0000

    Added event retargeting

--- component/ReactShadow.js
+++ component/ReactShadow.js
@@ -8,34 +8,96 @@
     $window.ReactShadow = {
 
         /**
+         * @property shadowRoot
+         * @type {Object}
+         */
+        shadowRoot: {},
+
+        /**
          * @method componentDidMount
          * @return {void}
          */
         componentDidMount: function componentDidMount() {
 
-            var shadowRoot      = this.getDOMNode().createShadowRoot(),
-                templateElement = $document.createElement('template');
+            var shadowRoot      = this.shadowRoot = this.getDOMNode().parentNode.createShadowRoot(),
+                templateElement = $document.createElement('template'),
+                rendered        = this.render();
+
+            // Obtain the HTML from the component's `render` method.
+            //$react.render(rendered, templateElement);
+            templateElement.content.appendChild(this.getDOMNode().cloneNode(true));
+            this.attachCSSDocuments(templateElement);
+
+            // Append the template node's content to our component.
+            var clone = $document.importNode(templateElement.content, true);
+            shadowRoot.appendChild(clone);
+            this.interceptEvents();
+
+        },
+
+        /**
+         * @method interceptEvents
+         * @return {void}
+         */
+        interceptEvents: function interceptEvents() {
+
+            this.shadowRoot.addEventListener('click', function onClick(event) {
+
+                event.stopPropagation();
+                event.preventDefault();
+
+                var targetId = event.target.getAttribute('data-reactid'),
+                    element  = $document.querySelector('*[data-reactid="' + targetId + '"]');
+
+                element.click();
+
+            }.bind(this));
+
+        },
+
+        /**
+         * @method componentDidUpdate
+         * @return {void}
+         */
+        componentDidUpdate: function componentDidUpdate() {
+
+            var containerElement = this.shadowRoot.querySelector(':not(style)');
+            containerElement.innerHTML = '';
+
+            var domNode    = this.getDOMNode(),
+                children   = domNode.children,
+                childCount = children.length;
+
+            for (var index = 0; index < childCount; index++) {
+                containerElement.appendChild(domNode.children[index].cloneNode(true));
+            }
+
+        },
+
+        /**
+         * @method attachCSSDocuments
+         * @param element {HTMLElement}
+         * @return {HTMLElement}
+         */
+        attachCSSDocuments: function attachCSSDocuments(element) {
 
             if (this.cssDocuments) {
 
-                this.cssDocuments.forEach(function forEach(cssDocument) {
+                var isFunction   = typeof this.cssDocuments === 'function',
+                    cssDocuments = isFunction ? this.cssDocuments() : this.cssDocuments;
+
+                cssDocuments.forEach(function forEach(cssDocument) {
 
                     // Construct the HTML for the external stylesheets.
                     var styleElement = $document.createElement('style');
                     styleElement.innerHTML = '@import "' + cssDocument + '"';
-                    templateElement.content.appendChild(styleElement);
+                    element.content.appendChild(styleElement);
 
                 });
 
             }
 
-            // Obtain the HTML from the component's `render` method.
-            templateElement.innerHTML += $react.renderToString(this.render());
-
-            // Append the template node's content to our component.
-            var clone = $document.importNode(templateElement.content, true);
-            shadowRoot.appendChild(clone);
-            this.cleanElements();
+            return element;
 
         },
 
--- example/js/App.jsx
+++ example/js/App.jsx
@@ -17,7 +17,15 @@
          * @property cssDocuments
          * @type {Array}
          */
-        cssDocuments: ['../css/Default.css'],
+        //cssDocuments: ['../css/Default.css'],
+
+        /**
+         * @method cssDocuments
+         * @return {*[]}
+         */
+        cssDocuments: function cssDocuments() {
+            return ['../css/Component.css', '../css/' + this.props.cssDocument];
+        },
 
         /**
          * @method getInitialState
@@ -32,11 +40,31 @@
          * @return {void}
          */
         componentDidMount: function componentDidMount() {
+            this.startInterval();
+        },
 
-            setTimeout(function() {
+        /**
+         * @method startInterval
+         * @return {void}
+         */
+        startInterval: function startInterval() {
+
+            var interval = setInterval(function() {
                 this.setState({ refreshed: this.state.refreshed + 1 });
             }.bind(this), 1000);
 
+            this.setState({ interval: interval });
+
+        },
+
+        /**
+         * @method reset
+         * @return {void}
+         */
+        reset: function reset() {
+            clearInterval(this.state.interval);
+            this.setState({ refreshed: 0 });
+            this.startInterval();
         },
 
         /**
@@ -44,13 +72,21 @@
          * @return {XML}
          */
         render: function render() {
-            return <div><h1 className="title">ReactShadow Example: {this.state.refreshed}</h1></div>
+            return <section>
+                       <h1 className="title">ReactShadow Example: {this.state.refreshed}</h1>
+                       <a title="Reset Counter" className="reset" onClick={this.reset}>Reset Counter</a>
+                   </section>
         }
 
     });
 
     // Mount the node into the DOM!
-    var mountNode = document.querySelector('*[data-react-shadow]');
-    $react.render(<AppExample></AppExample>, mountNode);
+    var firstMountNode  = document.querySelector('*[data-react-shadow="first"]'),
+        secondMountNode = document.querySelector('*[data-react-shadow="second"]'),
+        thirdMountNode  = document.querySelector('*[data-react-shadow="third"]');
+
+    $react.render(<AppExample cssDocument="component/First.css"></AppExample>, firstMountNode);
+    //$react.render(<AppExample cssDocument="component/Second.css"></AppExample>, secondMountNode);
+    //$react.render(<AppExample cssDocument="component/Third.css"></AppExample>, thirdMountNode);
 
 })(window.React);
