1db4b63c9aaa42170b9ff29e32e748eeca962d7c
Wildhoney <adam.timberlake@gmail.com>
2015-02-13 11:34:49 +0000

    Build

--- dist/react-shadow.js
+++ dist/react-shadow.js
@@ -1,4 +1,4 @@
-(function main($window) {
+(function main($window, $document, $react) {
 
     /**
      * @module ReactShadow
@@ -8,24 +8,114 @@
     $window.ReactShadow = {
 
         /**
+         * @property shadowRoot
+         * @type {Object}
+         */
+        shadowRoot: {},
+
+        /**
          * @method componentDidMount
          * @return {void}
          */
         componentDidMount: function componentDidMount() {
 
-            var shadow = this.getDOMNode().createShadowRoot();
-            var html = $react.renderToString(this.render());
+            var shadowRoot      = this.shadowRoot = this.getDOMNode().parentNode.createShadowRoot(),
+                templateElement = $document.createElement('template'),
+                rendered        = this.render();
 
-            var template = document.createElement('template');
-            template.innerHTML = '<style> @import "../css/Default.css"; </style>' + html;
+            // Obtain the HTML from the component's `render` method.
+            //$react.render(rendered, templateElement);
+            templateElement.content.appendChild(this.getDOMNode().cloneNode(true));
+            this.attachCSSDocuments(templateElement);
 
-            var clone = document.importNode(template.content, true);
-            shadow.appendChild(clone);
-
-            //root.innerHTML = html;
+            // Append the template node's content to our component.
+            var clone = $document.importNode(templateElement.content, true);
+            shadowRoot.appendChild(clone);
+            this.interceptEvents();
 
         },
 
+        /**
+         * @method interceptEvents
+         * @return {void}
+         */
+        interceptEvents: function interceptEvents() {
+
+            this.shadowRoot.addEventListener('click', function onClick(event) {
+
+                event.stopPropagation();
+                event.preventDefault();
+
+                var targetId = event.target.getAttribute('data-reactid'),
+                    element  = $document.querySelector('*[data-reactid="' + targetId + '"]');
+
+                element.click();
+
+            }.bind(this));
+
+        },
+
+        /**
+         * @method componentDidUpdate
+         * @return {void}
+         */
+        componentDidUpdate: function componentDidUpdate() {
+
+            var containerElement = this.shadowRoot.querySelector(':not(style)');
+            containerElement.innerHTML = '';
+
+            var domNode    = this.getDOMNode(),
+                children   = domNode.children,
+                childCount = children.length;
+
+            for (var index = 0; index < childCount; index++) {
+                containerElement.appendChild(domNode.children[index].cloneNode(true));
+            }
+
+        },
+
+        /**
+         * @method attachCSSDocuments
+         * @param element {HTMLElement}
+         * @return {HTMLElement}
+         */
+        attachCSSDocuments: function attachCSSDocuments(element) {
+
+            if (this.cssDocuments) {
+
+                var isFunction   = typeof this.cssDocuments === 'function',
+                    cssDocuments = isFunction ? this.cssDocuments() : this.cssDocuments;
+
+                cssDocuments.forEach(function forEach(cssDocument) {
+
+                    // Construct the HTML for the external stylesheets.
+                    var styleElement = $document.createElement('style');
+                    styleElement.innerHTML = '@import "' + cssDocument + '"';
+                    element.content.appendChild(styleElement);
+
+                });
+
+            }
+
+            return element;
+
+        },
+
+        /**
+         * @method cleanElements
+         * @return {void}
+         */
+        cleanElements: function cleanElements() {
+
+            var children = this.getDOMNode().children,
+                count    = children.length;
+
+            while (count--) {
+                children[count].remove();
+            }
+
+        }
+
     };
 
-})(window);
+})(window, window.document, window.React);
