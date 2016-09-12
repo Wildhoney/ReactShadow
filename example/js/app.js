e2b4b9ae51d786c268262047e5b1b66f568bc62f
Wildhoney <adam.timberlake@gmail.com>
2016-09-12 14:38:08 +0100

    Updated example for <ShadowDOM /> implementation

--- example/js/app.js
+++ example/js/app.js
@@ -1,100 +1,84 @@
-(function main($react) {
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+import ready from 'document-ready-promise';
+import ShadowDOM from '../../src/react-shadow';
+
+/**
+ * @class ReactShadow
+ * @author Adam Timberlake
+ * @link https://github.com/Wildhoney/ReactShadow
+ */
+export class Counter extends Component {
 
     /**
-     * @class ReactShadow
-     * @author Adam Timberlake
-     * @link https://github.com/Wildhoney/ReactShadow
+     * @constant propTypes
+     * @type {Object}
      */
-    var AppExample = $react.createClass({
+    static propTypes = {
+        name: PropTypes.string.isRequired
+    };
 
-        /**
-         * @property mixins
-         * @type {Array}
-         */
-        mixins: [ReactShadow],
+    /**
+     * @method constructor
+     * @return {Object}
+     */
+    constructor() {
+        super();
+        this.state = { refreshed: 0, value: '' };
+    }
 
-        /**
-         * @property cssDocuments
-         * @type {Array}
-         */
-        //cssDocuments: ['../css/Default.css'],
+    /**
+     * @method componentDidMount
+     * @return {void}
+     */
+    componentDidMount() {
+        this.startInterval();
+    }
 
-        /**
-         * @method cssDocuments
-         * @return {*[]}
-         */
-        cssDocuments: function cssDocuments() {
-            return ['../css/Component.css', '../css/' + this.props.cssDocument];
-        },
+    /**
+     * @method startInterval
+     * @return {void}
+     */
+    startInterval() {
+        const interval = setInterval(() => this.setState({ refreshed: this.state.refreshed + 1 }), 1000);
+        this.setState({ interval: interval });
+    }
 
-        /**
-         * @property cssSource
-         */
-        cssSource: "section { box-shadow: 2px 2px black }",
+    /**
+     * @method resetCounter
+     * @return {void}
+     */
+    resetCounter() {
+        clearInterval(this.state.interval);
+        this.setState({ refreshed: 0, value: this.state.value });
+        this.startInterval();
+    }
 
-        /**
-         * @method getInitialState
-         * @return {{refreshed: number}}
-         */
-        getInitialState: function getInitialState() {
-            return { refreshed: 0, value: '' };
-        },
+    /**
+     * @method render
+     * @return {XML}
+     */
+    render() {
 
-        /**
-         * @method componentDidMount
-         * @return {void}
-         */
-        componentDidMount: function componentDidMount() {
-            this.startInterval();
-        },
+        return (
+            <ShadowDOM cssDocuments={['css/core.css', `css/component/${this.props.name}.css`]}>
+                <section onClick={this.resetCounter.bind(this)} title="Reset Counter">
+                    <h1 className="title">
+                        {this.state.refreshed}
+                    </h1>
+                </section>
+            </ShadowDOM>
+        );
 
-        /**
-         * @method startInterval
-         * @return {void}
-         */
-        startInterval: function startInterval() {
+    }
 
-            var interval = setInterval(function() {
-                this.setState({ refreshed: this.state.refreshed + 1 });
-            }.bind(this), 1000);
+}
 
-            this.setState({ interval: interval });
+ready().then(() => {
 
-        },
-
-        /**
-         * @method resetCounter
-         * @return {void}
-         */
-        resetCounter: function resetCounter() {
-            clearInterval(this.state.interval);
-            this.setState({ refreshed: 0, value: this.state.value });
-            this.startInterval();
-        },
-
-        /**
-         * @method render
-         * @return {XML}
-         */
-        render: function render() {
-
-            return <section onClick={this.resetCounter} title="Reset Counter">
-                       <h1 className="title" onClick={this.updateName}>
-                           {this.state.refreshed}
-                       </h1>
-                   </section>
-
-        }
-
+    ['first', 'second', 'third'].forEach(name => {
+        const node = document.querySelector(`*[data-react-shadow="${name}"]`) || document.body;
+        render(<Counter name={name} />, node);
     });
 
-    // Mount the node into the DOM!
-    var firstMountNode  = document.querySelector('*[data-react-shadow="first"]'),
-        secondMountNode = document.querySelector('*[data-react-shadow="second"]'),
-        thirdMountNode  = document.querySelector('*[data-react-shadow="third"]');
-
-    $react.render(<AppExample cssDocument="component/First.css"></AppExample>, firstMountNode);
-    $react.render(<AppExample cssDocument="component/Second.css"></AppExample>, secondMountNode);
-    $react.render(<AppExample cssDocument="component/Third.css"></AppExample>, thirdMountNode);
-
-})(window.React);
+});
