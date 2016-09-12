950cc28c9e027ebf021408e0687a1c22993cca3f
Wildhoney <adam.timberlake@gmail.com>
2016-09-12 14:37:56 +0100

    Using <ShadowDOM /> component from Keo module

--- src/react-shadow.js
+++ src/react-shadow.js
@@ -1,235 +1,144 @@
-(function main($window, $document) {
+import { get as fetch } from 'axios';
+import React, { Component, PropTypes, createElement, DOM } from 'react';
+import { render, findDOMNode } from 'react-dom';
+import { dissoc } from 'ramda';
 
-    "use strict";
-
-    var React;
-    var ReactDOM;
-
+/**
+ * @class ShadowDOM
+ * @extends Component
+ */
+export default class ShadowDOM extends Component {
 
     /**
-     * @constant REACT_ID_ATTRIBUTE
-     * @type {String}
+     * @constant propTypes
+     * @type {Object}
      */
+    static propTypes = {
+        children: PropTypes.node.isRequired,
+        cssDocuments: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
+    };
 
-    var REACT_ID_ATTRIBUTE = 'data-reactid';
-
-    if (typeof require === 'function') {
-        React = require('react');
-        ReactDOM = require('react-dom');
-        REACT_ID_ATTRIBUTE = require('react/lib/DOMProperty').ID_ATTRIBUTE_NAME;
-    } else {
-        React = $window.React;
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+        this.state = { resolving: false };
     }
 
     /**
-     * @constant REACT_SHADOW_ROOT
-     * @type {String}
+     * @method getContainer
+     * @return {Object}
      */
-    var REACT_SHADOW_ROOT = 'main';
+    getContainer() {
+
+        // Wrap children in a container if it's an array of children, otherwise
+        // simply render the single child which is a valid `ReactElement` instance.
+        const children = this.props.children.props.children;
+        return children.length ? <span>{children}</span> : children;
+
+    }
 
     /**
-     * @module ReactShadow
-     * @author Adam Timberlake
-     * @link https://github.com/Wildhoney/ReactShadow
+     * @method componentDidMount
+     * @return {void}
      */
-    var ReactShadow = {
+    componentDidMount() {
 
-        /**
-         * @property _shadowRoot
-         * @type {Object}
-         * @private
-         */
-        _shadowRoot: {},
+        // Create the shadow root and take the CSS documents from props.
+        const node = findDOMNode(this);
+        const root = node.attachShadow ? node.attachShadow({ mode: open }) : node.createShadowRoot();
+        const cssDocuments = this.props.cssDocuments || [];
+        const container = this.getContainer();
 
-        /**
-         * @method componentDidMount
-         * @return {void}
-         */
-        componentDidMount: function componentDidMount() {
+        // Render the passed in component to the shadow root, and then `setState` if there
+        // are no CSS documents to be resolved.
+        render(container, root);
+        !cssDocuments && this.setState({ root });
 
-            var shadowRoot  = this._shadowRoot = ReactDOM.findDOMNode(this.parentNode).createShadowRoot(),
-                mainElement = $document.createElement(REACT_SHADOW_ROOT);
+        if (cssDocuments.length) {
 
-            // Attach CSS.
-            this._attachCSS(shadowRoot);
-
-            // Append the template node's content to our component.
-            shadowRoot.appendChild(mainElement);
-
-            // Render component and intercept the DOM events.
-            React.render(this.render(), mainElement);
-            this._interceptEvents();
-
-            // Strip inline <style> elements
-            this._stripInlineStyles();
-
-            // Wrap current DOM node in `script` tag.
-            var scriptElement = $document.createElement('script');
-            ReactDOM.findDOMNode(this).parentNode.appendChild(scriptElement);
-            scriptElement.appendChild(ReactDOM.findDOMNode(this));
-
-            // Shadow insertion point for nesting shadow roots.
-            //shadowRoot.appendChild($document.createElement('shadow'));
-
-        },
-
-        /**
-         * @method componentDidUpdate
-         * @return {void}
-         */
-        componentDidUpdate: function componentDidUpdate() {
-            var containerElement = this._shadowRoot.querySelector(REACT_SHADOW_ROOT);
-            React.render(this.render(), containerElement);
-            this._stripInlineStyles();
-        },
-
-
-       /**
-        * @method _stripInlineStyles
-        * @return {void}
-        * @private
-        */
-        _stripInlineStyles: function _stripInlineStyles() {
-            var src = ReactDOM.findDOMNode(this);
-            var s = src.querySelectorAll('style');
-            var i = s.length;
-            while (i--) { s[i].remove(); }
-        },
-
-        /**
-         * @method _attachCSS
-         * @param {HTMLElement} shadowRoot
-         * @return {void}
-         * @private
-         */
-        _attachCSS: function _attachCSS(shadowRoot) {
-
-            if (this.cssDocuments) {
-                this._attachCSSDocuments(shadowRoot);
-            }
-
-            if (this.cssSource) {
-                this._attachCSSSource(shadowRoot);
-            }
-
-        },
-
-        /**
-         * @method _interceptEvents
-         * @return {void}
-         * @private
-         */
-        _interceptEvents: function _interceptEvents() {
-
-            // Memorise the React ID's root ID for intercepting events.
-            var rootReactId = ReactDOM.findDOMNode(this).getAttribute(REACT_ID_ATTRIBUTE),
-                domNode     = ReactDOM.findDOMNode(this).parentNode;
-
-            /**
-             * @method redirectEvent
-             * @param event {Object}
-             * @return {void}
-             */
-            var redirectEvent = function redirectEvent(event) {
-
-                event.stopPropagation();
-
-                var targetId = event.target.getAttribute(REACT_ID_ATTRIBUTE);
-
-                if (targetId) {
-
-                    // Translate current target ID into the React.js element we're shadowing.
-                    var translatedId = targetId.replace(/\.[0-9]+/, rootReactId),
-                        element      = domNode.querySelector('*[' + REACT_ID_ATTRIBUTE + '="' + translatedId + '"]');
-
-                    if (event.target.value) {
-
-                        // Update the original element's value.
-                        element.setAttribute('value', event.target.value);
-
-                    }
-
-                    // Dispatch the event on the original component's element.
-                    var customEvent = $document.createEvent('Events');
-                    customEvent.initEvent(event.type, true, false);
-                    element.dispatchEvent(customEvent);
-
-                }
-
-            }.bind(this);
-
-            // List of all events that should be intercepted and re-routed.
-            var eventsList = ['click', 'dblclick', 'mouseup', 'mouseout', 'mouseover', 'mousedown', 'mouseenter',
-                              'mouseleave', 'contextmenu', 'keyup', 'keydown', 'change'];
-
-            eventsList.forEach(function forEach(eventName) {
-                this._shadowRoot.addEventListener(eventName, redirectEvent);
-            }.bind(this));
-
-        },
-
-        /**
-         * Construct the HTML for the external stylesheets
-         *
-         * @method createStyle
-         * @param {HTMLElement} element
-         * @param {String} styleContent Content style for given element.
-         * @return {HTMLElement}
-         * @private
-         */
-        _createStyle: function(element, styleContent) {
-
-            var styleElement       = $document.createElement('style');
-            styleElement.innerHTML = styleContent;
-            element.appendChild(styleElement);
-            return element;
-
-        },
-
-        /**
-         * @method _attachCSSSource
-         * @param  {HTMLElement} element
-         * @return {HTMLElement}
-         * @private
-         */
-        _attachCSSSource: function(element) {
-            this._createStyle(element, this.cssSource);
-        },
-
-
-        /**
-         * @method _attachCSSDocuments
-         * @param element {HTMLElement}
-         * @return {HTMLElement}
-         * @private
-         */
-        _attachCSSDocuments: function _attachCSSDocuments(element) {
-            var that = this;
-
-            if (this.cssDocuments) {
-
-                var isFunction   = typeof this.cssDocuments === 'function',
-                    cssDocuments = isFunction ? this.cssDocuments() : this.cssDocuments;
-
-                cssDocuments.forEach(function forEach(cssDocument) {
-                    that._createStyle(element, '@import "' + cssDocument + '"');
-                });
-
-            }
-
-            return element;
+            // Otherwise we'll fetch and attach the passed in stylesheets which need to be
+            // resolved before `state.resolved` becomes `true` again.
+            this.setState({ resolving: true, root });
+            this.attachStylesheets(this.props.cssDocuments);
 
         }
 
-    };
-
-
-    // Export the module as a CommonJS module if possible
-    if (typeof module !== 'undefined' && module != null) {
-        module.exports = ReactShadow;
-        return;
     }
 
-    $window.ReactShadow = ReactShadow;
+    /**
+     * @method componentDidUpdate
+     * @return {void}
+     */
+    componentDidUpdate() {
 
-})(window, window.document);
+        // Updates consist of simply rendering the container element into the shadow root
+        // again, as the `this.getContainer()` element contains the passed in component's
+        // children.
+        render(this.getContainer(), this.state.root);
+
+    }
+
+    /**
+     * @method attachStylesheets
+     * @param cssDocuments {Array|String}
+     * @return {void}
+     */
+    attachStylesheets(cssDocuments) {
+
+        const styleElement = document.createElement('style');
+        styleElement.setAttribute('type', 'text/css');
+        const documents = Array.isArray(cssDocuments) ? cssDocuments : [cssDocuments];
+
+        /**
+         * @method fetchStylesheet
+         * @param {String} document
+         * @return {Promise}
+         */
+        const fetchStylesheet = document => fetch(document).then(response => response.data);
+
+        /**
+         * @method insertStyleElement
+         * @param {Array} cssDocuments
+         * @return {void}
+         */
+        const insertStyleElement = cssDocuments => {
+
+            styleElement.innerHTML = cssDocuments.reduce((accumulator, document) => {
+                return `${accumulator} ${document}`;
+            });
+
+            this.state.root.appendChild(styleElement);
+
+        };
+
+        Promise.all(documents.map(fetchStylesheet)).then(cssDocuments => {
+            insertStyleElement(cssDocuments);
+            this.setState({ resolving: false });
+        });
+
+    }
+
+    /**
+     * @method render
+     * @return {XML}
+     */
+    render() {
+
+        // Take all of the props from the passed in component, minus the `children` props
+        // as that's handled by `componentDidMount`.
+        const props = dissoc('children', this.props.children.props);
+        const className = this.state.resolving ? 'resolving' : 'resolved';
+
+        // Determine whether to use `class` or `className`, as custom elements do not allow
+        // for `className`. See: https://github.com/facebook/react/issues/4933
+        const classNames = `${props.className ? props.className : ''} ${className}`.trim();
+        const isSupportedElement = this.props.children.type in DOM;
+        const propName = isSupportedElement ? 'className' : 'class';
+
+        return <this.props.children.type {...{ ...dissoc('className', props), [propName]: classNames }} />;
+
+    }
+
+}
