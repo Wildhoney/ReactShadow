a83940cd75a43fb5b7bb681d5d6b658ee942e0d3
Wildhoney <adam.timberlake@gmail.com>
2019-06-16 17:44:19 +0100

    Added rollup config

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+module.exports = {
+    input: 'src/index.js',
+    external: ['react', 'react-dom', 'prop-types'],
+    output: [
+        {
+            file: 'dist/react-shadow.cjs.js',
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'named',
+        },
+        {
+            file: 'dist/react-shadow.esm.js',
+            format: 'esm',
+            sourcemap: true,
+            exports: 'named',
+        },
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**',
+            runtimeHelpers: true,
+        }),
+        commonjs({
+            namedExports: {
+                include: 'node_modules/**',
+                'node_modules/react/index.js': [
+                    'cloneElement',
+                    'createContext',
+                    'Component',
+                    'createElement',
+                    'useRef',
+                    'useState',
+                    'useCallback',
+                    'useContext',
+                    'useEffect',
+                ],
+                'node_modules/react-is/index.js': [
+                    'isElement',
+                    'isValidElementType',
+                    'ForwardRef',
+                ],
+                'node_modules/styled-components/dist/styled-components.esm.js': [
+                    'createContext',
+                ],
+            },
+        }),
+        terser(),
+    ],
+};
