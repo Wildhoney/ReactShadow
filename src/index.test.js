dd1b6847ec651229f18d384fd81d9cd290fbf9f8
Wildhoney <adam.timberlake@gmail.com>
2019-06-17 18:59:41 +0100

    100% coverage for test suite

--- src/index.test.js
+++ src/index.test.js
@@ -17,6 +17,46 @@ test('It should be able to create the shadow boundary with custom elements;', t 
     t.is(wrapper.getDOMNode().shadowRoot.innerHTML, 'Hello Adam!');
 });
 
+test('It should be able to attach stylesheets to the shadow boundary;', t => {
+    const wrapper = mount(
+        <root.todoApp styleSheets={['index.css', 'person.css']}>
+            Hello Maria!
+        </root.todoApp>,
+    );
+    t.deepEqual(wrapper.getDOMNode().shadowRoot.adoptedStyleSheets, [
+        'index.css',
+        'person.css',
+    ]);
+});
+
+test('It should be able to pass options to the shadow boundary creation;', t => {
+    sinon.spy(window.HTMLElement.prototype, 'attachShadow');
+
+    mount(<root.todoApp>Hello Adam!</root.todoApp>);
+    t.is(window.HTMLElement.prototype.attachShadow.callCount, 1);
+    t.true(
+        window.HTMLElement.prototype.attachShadow.calledWith({
+            mode: 'open',
+            delegatesFocus: false,
+        }),
+    );
+
+    mount(
+        <root.todoApp mode="closed" delegatesFocus>
+            Hello Adam!
+        </root.todoApp>,
+    );
+    t.is(window.HTMLElement.prototype.attachShadow.callCount, 2);
+    t.true(
+        window.HTMLElement.prototype.attachShadow.calledWith({
+            mode: 'closed',
+            delegatesFocus: true,
+        }),
+    );
+
+    window.HTMLElement.prototype.attachShadow.restore();
+});
+
 test('It should be able to register events in the shadow boundary;', t => {
     const spies = { onClick: sinon.spy() };
     const wrapper = mount(
@@ -30,7 +70,7 @@ test('It should be able to register events in the shadow boundary;', t => {
     t.true(spies.onClick.calledWith('Maria'));
 });
 
-test('It should be able to applie styles to the shadow boundary components;', t => {
+test('It should be able to apply styles to the shadow boundary components;', t => {
     const wrapper = mount(
         <root.div>
             Hello Adam!
