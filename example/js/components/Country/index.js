60d50bee796f14220f4ad8d13d462e6bc39e86ed
Wildhoney <adam.timberlake@gmail.com>
2019-06-22 13:57:13 +0100

    Deps

--- example/js/components/Country/index.js
+++ example/js/components/Country/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
