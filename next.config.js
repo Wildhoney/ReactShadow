006b436b405ac5409c1093a6de93ce6e1496a2c6
Wildhoney <adam.timberlake@gmail.com>
2020-03-30 19:11:11 +0100

    Improved build of src dir

--- next.config.js
+++ next.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
 
-const aliases = [{ module: 'react-shadow', path: 'example/react-shadow.js' }];
+const aliases = [{ module: 'react-shadow', path: 'src/index.js' }];
 
 module.exports = {
     target: 'serverless',
-    webpack: (config) => {
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.css$/,
+            loader: ['to-string-loader', 'css-loader'],
+        });
+
+        config.module.rules.push({
+            test: /\.(js|jsx)$/,
+            include: [path.resolve(__dirname, './src')],
+            use: [options.defaultLoaders.babel],
+        });
+
         aliases.forEach((alias) => {
             config.resolve.alias[alias.module] = path.join(
                 __dirname,
                 alias.path,
             );
-        });
-
-        config.module.rules.push({
-            test: /\.css$/,
-            loader: ['to-string-loader', 'css-loader'],
         });
 
         return config;
