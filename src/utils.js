ab955c1af62a67a61d7721452c524115743493e5
Wildhoney <adam.timberlake@gmail.com>
2020-04-09 16:04:21 +0100

    Removed unused util functions

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,6 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 export const Context = createContext(null);
-
-function Noop({ children }) {
-    return children;
-}
-
-export function getStyleWrapper() {
-    try {
-        const styled = require('styled-components');
-        if (!('StyleSheetManager' in styled))
-            throw new Error(
-                '`StyleSheetManager` not found in `styled-components` library.',
-            );
-        return styled.StyleSheetManager;
-    } catch {
-        return Noop;
-    }
-}
-
-export function getStyleElements(children) {
-    try {
-        const reactDom = require('react-dom/server');
-        const styled = require('styled-components');
-        const sheet = new styled.ServerStyleSheet();
-
-        reactDom.renderToString(
-            <styled.StyleSheetManager sheet={sheet.instance}>
-                {children}
-            </styled.StyleSheetManager>,
-        );
-
-        return sheet.getStyleElement();
-    } catch {
-        return Noop;
-    }
-}
 
 export function handleError({ error, styleSheets, root }) {
     switch (error.name) {
