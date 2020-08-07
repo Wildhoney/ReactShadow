b9663c3a9cec9a8db00b91ec2d1d7269703ada3c
Wildhoney <adam.timberlake@gmail.com>
2020-08-07 19:30:33 +0100

    Added additional modules to the type file. Relates to #87

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,47 @@
-declare module "react-shadow" {
-  import * as React from "react";
+declare module 'react-shadow' {
+    import * as React from 'react';
 
-  type Root = {
-    [name: string]: React.ComponentType;
-  };
+    type Root = {
+        [name: string]: React.ComponentType;
+    };
 
-  const ReactShadowRoot: Root;
+    const ReactShadowRoot: Root;
 
-  export default ReactShadowRoot;
+    export default ReactShadowRoot;
+}
+
+declare module 'react-shadow/styled-components' {
+    import * as React from 'react';
+
+    type Root = {
+        [name: string]: React.ComponentType;
+    };
+
+    const ReactShadowRoot: Root;
+
+    export default ReactShadowRoot;
+}
+
+declare module 'react-shadow/emotion' {
+    import * as React from 'react';
+
+    type Root = {
+        [name: string]: React.ComponentType;
+    };
+
+    const ReactShadowRoot: Root;
+
+    export default ReactShadowRoot;
+}
+
+declare module 'react-shadow/material-ui' {
+    import * as React from 'react';
+
+    type Root = {
+        [name: string]: React.ComponentType;
+    };
+
+    const ReactShadowRoot: Root;
+
+    export default ReactShadowRoot;
 }
